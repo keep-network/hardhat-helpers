8b5bc7956b30eeab24aea626ac8b1386de20bfdb
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-11-19 11:40:46 +0100

    Attach helpers to hre.helpers

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { extendEnvironment } from "hardhat/config"
 import { lazyObject } from "hardhat/plugins"
 
-import { Address } from "./Address"
-import { Ownable } from "./Ownable"
+import account from "./account"
+import * as address from "./address"
+import forking from "./forking"
+import * as number from "./number"
+import ownable from "./ownable"
 import time from "./time"
 
 import "./type-extensions"
@@ -10,11 +13,20 @@ import "./type-extensions"
 extendEnvironment((hre) => {
   hre.helpers = lazyObject(() => {
     return {
+      account: lazyObject(() => {
+        return account(hre)
+      }),
       address: lazyObject(() => {
-        return new Address()
+        return address
+      }),
+      forking: lazyObject(() => {
+        return forking(hre)
+      }),
+      number: lazyObject(() => {
+        return number
       }),
       ownable: lazyObject(() => {
-        return new Ownable(hre)
+        return ownable(hre)
       }),
       time: lazyObject(() => {
         return time(hre)
